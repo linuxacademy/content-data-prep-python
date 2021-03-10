ca7991c0d5d849a2301cdbd7e243028898d334fc
david thomas <david.thomas@linuxacademy.com>
2021-03-10 14:50:43 -0500

    create a db connection

---
+++ Hands-On Labs/Using PostgreSQL/connect_db.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+DB_NAME = "demo"
+
+def get_database_connection():
+    """
+    this function should create a connection
+    to the database and return the connection
+    """
+
+    pass
+
