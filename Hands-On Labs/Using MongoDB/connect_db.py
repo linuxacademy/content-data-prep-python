fc64879b23bd79652b46ee83e6815383d04c7f5d
david thomas <david.thomas@linuxacademy.com>
2021-03-12 12:55:18 -0500

    connection info

---
+++ Hands-On Labs/Using MongoDB/connect_db.py
@@ -0,0 +1,12 @@
+import pymongo
+
+DB_NAME = "contracts"
+
+def get_database_connection():
+    """
+    this function should create a connection
+    to the database and return the connection
+    """
+
+    pass
+
